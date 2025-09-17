cc6144676a1bf4c11fb11e3854d61f6bdcc4af5d
jonathan-matheus <johhn.dev@gmail.com>
2025-09-17 13:25:34 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Meu Portfólio</title>
+  <link href="./src/output.css" rel="stylesheet">
+</head>
+
+<body class="bg-slate-900 text-white">
+  <header class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
+    <!-- logo -->
+    <div class="font-bold font-mono text-cyan-600">
+      Meu Portfólio
+    </div>
+
+    <!-- Links -->
+    <div>
+      <ul class="flex gap-3 font-medium text-gray-200">
+        <li><a href="#projetos" class="hover:underline">Projetos</a></li>
+        <li><a href="#github" class="hover:underline">GitHub</a></li>
+        <li><a href="#linkedin" class="hover:underline">LinkedIn</a></li>
+        <li><a href="#instagram" class="hover:underline">Instagram</a></li>
+      </ul>
+    </div>
+  </header>
+</body>
+
+</html>
