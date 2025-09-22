0dff4f22c45265173316d9271688bd98642cf8a7
jonathan-matheus <johhn.dev@gmail.com>
2025-09-21 22:17:01 -0300

    estelisando a main

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
   <link href="./src/output.css" rel="stylesheet">
 </head>
 
-<body class="bg-slate-900 text-white">
+<body class="bg-slate-900 text-gray-200">
+  <!-- Header -->
   <header class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
     <!-- logo -->
     <div class="font-bold font-mono text-cyan-600">
@@ -17,7 +18,7 @@
 
     <!-- Links -->
     <div>
-      <ul class="flex gap-3 font-medium text-gray-200">
+      <ul class="flex gap-3 font-medium">
         <li><a href="#projetos" class="hover:underline">Projetos</a></li>
         <li><a href="#github" class="hover:underline">GitHub</a></li>
         <li><a href="#linkedin" class="hover:underline">LinkedIn</a></li>
@@ -25,6 +26,49 @@
       </ul>
     </div>
   </header>
+
+  <!-- Main -->
+  <main class="mx-auto max-w-screen-lg min-h-20 px-3 py-6">
+    <!-- Hero -->
+    <section class="flex gap-x-3">
+      <!-- Titulo e Descrição -->
+      <div class="w-2/3">
+        <h1 class="text-3xl font-bold">
+          Oi, eu sou Jonathan
+        </h1>
+        <p class="text-xl leading-6 mt-6">
+          Falando um pouco sobre mim, sou um desenvolvedor web apaixonado por
+          criar soluções inovadoras e eficientes. Tenho experiência em diversas
+          tecnologias e estou sempre buscando aprender mais para aprimorar
+          minhas habilidades.
+        </p>
+        <p>
+          <!-- Links de redes sociais -->
+        <ul class="flex gap-x-3 mt-3">
+          <li><a href="https://github.com/seuusuario" class="hover:underline">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/seuusuario" class="hover:underline">LinkedIn</a></li>
+          <li><a href="https://www.instagram.com/seuusuario" class="hover:underline">Instagram</a></li>
+          <li><a href="https://twitter.com/seuusuario" class="hover:underline">Twitter</a></li>
+        </ul>
+        </p>
+      </div>
+
+      <!-- Imagem -->
+      <div class="w-1/3 flex items-center justify-center">
+        <div>
+          foto
+        </div>
+      </div>
+    </section>
+
+    <!-- Projetos -->
+    <section>
+      <!-- Projetos -->
+      <div>
+
+      </div>
+    </section>
+  </main>
 </body>
 
 </html>
